cda3200cb3d0205ead4ed854464cdaef8b727639
Brian Bosak <brielcccc@gmail.com>
2018-02-20 21:50:27 -0600

    Misc. changes

--- xeditor.cpp
+++ xeditor.cpp
@@ -29,28 +29,50 @@ public:
         setLineWrapMode(LineWrapMode::NoWrap);
         ctx = compiler_new();
     }
-    std::string token_text; //Current text for token
     ScopeNode* token_scope = 0; //Current scope for token
     Node* token = 0; //Current token
-    size_t token_offset = 0; //Current offset into token
     //Parse and syntax-highlight code at current offset
+
+    QString toUnicode(const char* str) {
+        return str;
+    }
+    size_t unicodeSize(const char* str) {
+        return toUnicode(str).length();
+    }
+    std::string fromUnicode(QString str) {
+        QByteArray byteme = str.toUtf8();
+        return byteme.data();
+    }
+
+    QTextCursor token_select(Node* token) {
+       ExtendedTokenInfo* tinfo = (ExtendedTokenInfo*)token->ide_context;
+       QTextCursor retval = tinfo->start;
+       return retval;
+    }
+    QTextCursor changedText; //Text to reparse
     void parse() {
-        QTextCursor cursor = textCursor();
+        setTokenInfo();
+        std::string token_text = fromUnicode(changedText.selectedText());
         if(token) {
             delete token;
             token = 0;
         }
-        cursor.movePosition(QTextCursor::PreviousCharacter,QTextCursor::MoveAnchor,token_offset);
-        cursor.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor,token_text.size());
-        setTextCursor(cursor);
+
+        Node* empty[1];
+
         Node** nodes;
         size_t len;
         std::string errstr;
-        if(!ctx->parse(token_text.data(),token_scope,&nodes,&len)) {
-            //Error -- apply red underline
-            errstr = "Illegal expression";
-            goto token_err;
-        }else {
+        bool ean = ctx->parse(token_text.data(),token_scope,&nodes,&len);
+        if(!ean) {
+            nodes = empty;
+            len = 1;
+            nodes[0] = new Node(Invalid);
+            nodes[0]->move(token_text.data());
+        }
+
+
+
             const char* start = nodes[0]->location;
             size_t tsize = 0;
             for(size_t i = 0;i<len;i++) {
@@ -70,36 +92,20 @@ public:
                 std::stringstream ss;
                 ss<<(void*)token;
                 fmt.setAnchorName(ss.str().data());
+                if(token->type == Invalid) {
+                    fmt.setToolTip(errstr.data());
+                    fmt.setUnderlineStyle(QTextCharFormat::SpellCheckUnderline);
+                    fmt.setUnderlineColor(Qt::red);
+                    fmt.setFontUnderline(true);
+                }
                 QString str = fmt.anchorName();
                 tinfo->start = QTextCursor(document());
-                tinfo->start.setPosition(cursor.position());
-                char* insstr = (char*)start+(tsize-token->ide_token_size);
-                char prevchar = insstr[tsize];
-                insstr[tsize] = 0;
-                textCursor().insertText(insstr,fmt); //TODO: Get these cursors figured out. Also; should only insert
+                tinfo->start.setPosition(changedText.anchor()+(tsize-token->ide_token_size));
                 //the number of characters in the token somehow.
-                tinfo->start.movePosition(QTextCursor::PreviousCharacter,QTextCursor::MoveAnchor,token->ide_token_size);
+                tinfo->start.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor,token->ide_token_size); //Select complete block
                 int pos = tinfo->start.position();
-                insstr[tsize] = prevchar;
-            }
+                token_select(token).setCharFormat(fmt);
         }
-
-        cursor = textCursor();
-        cursor.movePosition(QTextCursor::PreviousCharacter,QTextCursor::MoveAnchor,token_text.size()-token_offset-1);
-        setTextCursor(cursor);
-
-        return;
-        token_err:
-        QTextCharFormat fmt;
-        fmt.setToolTip(errstr.data());
-        fmt.setUnderlineStyle(QTextCharFormat::SpellCheckUnderline);
-        fmt.setUnderlineColor(Qt::red);
-        fmt.setFontUnderline(true);
-        fmt.setAnchorName("");
-        textCursor().insertText(token_text.data(),fmt);
-        cursor = textCursor();
-        cursor.movePosition(QTextCursor::PreviousCharacter,QTextCursor::MoveAnchor,token_text.size()-token_offset-1);
-        setTextCursor(cursor);
     }
     void setTokenInfo() {
         QByteArray byteme = textCursor().charFormat().anchorName().toUtf8();
@@ -108,36 +114,8 @@ public:
         void* nin = 0;
         ss>>nin;
         Node* n = (Node*)nin;
-        if(n) {
-            token = n;
-            ExtendedTokenInfo* tinfo = (ExtendedTokenInfo*)token->ide_context;
-            token_offset = textCursor().position()-tinfo->start.position();
-            if(token_offset == token->ide_token_size) {
-                token = 0;
-                token_offset = 0;
-                return;
-            }
-            QTextCursor selection = tinfo->start;
-            selection.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor,token->ide_token_size);
-            QByteArray bitten = selection.selectedText().toUtf8();
-            token_text = bitten.data();
-        }
-    }
-    void insert(const char* text) {
-        size_t oldlen = token_text.size();
-        if(token_offset == token_text.size()) {
-            token_text+=text;
-            token_offset+=(token_text.size()-oldlen);
-            return;
-        }
-        std::stringstream ss;
-        char prev = token_text[token_offset];
-        token_text[token_offset] = 0;
-        ss<<token_text.data();
-        ss<<text;
-        token_text[token_offset] = prev;
-        ss<<(token_text.data()+token_offset);
-        token_text = ss.str();
+        token = n;
+
     }
 
     void keyPressEvent(QKeyEvent *e) {
@@ -172,19 +150,24 @@ public:
         default:
             //Insert one character of text
             e->accept();
-            QByteArray byteme = e->text().toUtf8();
-            insert(byteme.data());
+            textCursor().insertText(e->text());
+            if(token) {
+                if(token->type == Invalid) {
+                    //Invalid token -- set bounds
+                    changedText = ((ExtendedTokenInfo*)token->ide_context)->start;
+                    changedText.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor,1);
+                }else {
+                    changedText.setPosition(textCursor().position()-1);
+                    changedText.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor,1);
+                }
+            }else {
+                changedText = textCursor();
+                changedText.movePosition(QTextCursor::PreviousCharacter);
+                changedText.movePosition(QTextCursor::NextCharacter,QTextCursor::KeepAnchor,1);
+            }
+
             parse();
             break;
-        }
-        if(token) {
-        if(token_offset == token->ide_token_size) {
-                //End of token
-                token_offset = 0;
-                token = 0;
-                token_text.clear();
-                setTokenInfo();
-            }
         }
     }
     void mousePressEvent(QMouseEvent* evt) {
