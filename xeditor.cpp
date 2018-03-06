ce5a4fdec2ef6995b2bde10cb73478f3f6f77578
Brian Bosak <brielcccc@gmail.com>
2018-03-05 21:53:54 -0600

    Added license.

--- xeditor.cpp
+++ xeditor.cpp
@@ -1,3 +1,14 @@
+/*
+Copyright 2018 Brian Bosak
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
 #include "xeditor.h"
 #include <QPaintEvent>
 #include <QPainter>
@@ -60,6 +71,7 @@ public:
                 break;
             }
             prev = token;
+
             if(token) {
                 QTextCursor tokenpos = token_select(token);
                 tokenpos.setCharFormat(QTextCharFormat());
@@ -156,6 +168,10 @@ public:
                 int pos = tinfo->start.position();
                 token_select(token).setCharFormat(fmt);
         }
+
+
+            //Perform verification on each node
+
     }
     Node* findToken(QTextCursor cursor) {
         QByteArray byteme = textCursor().charFormat().anchorName().toUtf8();
@@ -166,7 +182,6 @@ public:
         Node* n = (Node*)nin;
         return n;
     }
-
 
     void keyPressEvent(QKeyEvent *e) {
 
@@ -195,6 +210,15 @@ public:
         case Qt::Key_Down:
             XEditor::keyPressEvent(e);
             break;
+        case Qt::Key_Backspace:
+        case Qt::Key_Delete:
+        {
+            QTextCursor changedText = getTotalChanges(textCursor());
+            deleteSelection(changedText);
+            XEditor::keyPressEvent(e);
+            parse(changedText);
+        }
+            break;
         default:
             //Insert one character of text
             e->accept();
@@ -212,6 +236,7 @@ public:
     }
     void mousePressEvent(QMouseEvent* evt) {
         evt->accept();
+
     }
     void contextMenuEvent(QContextMenuEvent* evt) {
         evt->accept();
